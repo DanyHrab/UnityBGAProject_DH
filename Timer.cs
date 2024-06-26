54db0cb1db8f9e529482b4708dad8552ad0e91d4
DanyHrab <daniel.hrab.bga@gmail.com>
2024-06-26 12:07:02 +0100

    Updated Timer.cs

--- Timer.cs
+++ Timer.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
 
     public float seconds = 59f;
-    //public float minutes = 2f;
+    public float minutes = 2;
+    public TextMeshProUGUI time;
 
     // Start is called before the first frame update
     void Start()
     {
 
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -23,15 +25,15 @@ public class Timer : MonoBehaviour
         {
             seconds -= Time.deltaTime;
         }
-        /*else if (minutes > 0)
+        else if (minutes > 0)
         {
             minutes -= 1;
             seconds = 59f;
-        }*/
+        }
         else
         {
             EditorSceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-        print(seconds);
+        time.text = minutes + ":" + Mathf.RoundToInt(seconds);
     }
 }
